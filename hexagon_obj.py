b4ec0075c6e277f0e4c099b74ecefd50764edb39
eshatkeinensinn <43652992+eshatkeinensinn@users.noreply.github.com>
2025-07-12 22:11:08 +0200

    Notes for spiral detection

--- hexagon_obj.py
+++ hexagon_obj.py
@@ -8,7 +8,7 @@ from shapely.affinity import rotate, scale
 import hex_structure
 
 class grid:
-    def __init__(self, width, height, hex_size, offset_x = 0, offset_y = 0, num = 5, background=True, margin_width=10, margin_height=10):
+    def __init__(self, width, height, hex_size, offset_x = 10, offset_y = 0, num = 5, background=True, margin_width=10, margin_height=10):
         self.width = width
         self.height = height
         self.offset_x = offset_x
@@ -39,10 +39,11 @@ class grid:
         row_index = 0
         for i in np.arange (0 - self.offset_y, self.height + self.hex_r_y, self.hex_r_y):
             
-            x_offset = 0 if row_index % 2 == 0 else 1.5 * self.hex_r_x - self.offset_x
+            x_offset = 0 if row_index % 2 == 0 else 1.5 * self.hex_r_x
 
             for j in np.arange(0 - self.offset_x + x_offset, self.width + self.hex_r_x, 3 * self.hex_r_x):
-                hex = hex_structure.Hexagon(j, i, self.hex_size)
+                id = [round((i + self.offset_y) / self.hex_r_y), round((j + self.offset_x - x_offset) / (3 * self.hex_r_x))]
+                hex = hex_structure.Hexagon(j, i, self.hex_size,id)
                 self.grid.append(hex)
 
 
@@ -51,6 +52,63 @@ class grid:
 
 
         self.draw_grid_one_colour()
+
+
+    '''
+    Die conncted Segment List
+    Es wird jedes Segment in jedem Hexagon in Grid durchgegangen
+
+        Überprüfen ob das Segment an Passende Stelle Passt
+            Wenn nur an eine offene Gruppe
+                Segment der Gruppe hinzufügen
+                Neues Offene Ende der Gruppe setzten
+            Wenn passend auf 2 neue Gruppen
+                Segment der ersten Gruppe hinzufügen
+                Segment der zweiten an die erste Gruppe anhängen
+                Neue offene Enden setzten
+            Wenn keine Gruppe
+                Eine Neue Gruppe erstellen mit Passenden Enden
+    
+    Testen der Segment Nach Verbindungen
+    offene_segmente = []
+        enthält Gruppen (vielleicht ein neues Objekt machen)
+         Eigenschaften
+            Liste mit Hexagon.ID und Hexagon.Connection Index
+            Offene Enden der Line (offene Kante auf die das Zugehörigen Hexagon z.b ([14, 2], 4) letzte Zugefügte Kante wäre 12,1 
+
+            Funktion die Ein Segment prüft ob Teil der Gruppe
+                Testen ob durch id und connection das segment anschließt 
+                        Schließt [14, 3] [[4, 5], [1, 2], [0, 3]] an ([14, 2], 1)
+            
+            Funktion die ein Segment in die Gruppe aufnimmt
+                Liste einfügen
+                Neues Ende setzten
+            
+            Funktion die eine Gruppe in die Gruppe aufnimmt
+                Liste einfügen
+                Neues Ende bestimmen
+            
+    
+
+    ???
+    Macht Testen nach Unmöglichen Enden aka Rändern Sinn? in dem Objekt Gruppe wahrscheinlich 
+
+
+
+    
+    
+    
+    '''
+
+    def connected_segments(self):
+        for i in range(0, len(self.grid), 1):
+            print(self.grid[i].id, self.grid[i].connection)
+            for connection in self.grid[i].connection:
+                for point in connection:
+                    if point == 1:
+                      print(1)  
+                    
+
 
     # a draw mode to get a svg with everything one colour
     def draw_grid_one_colour(self):
@@ -107,3 +165,4 @@ hex_size = 25  # Außendurchmesser, ggf. in mm anpassen
 x=8
 
 grid = grid(a4_width_mm, a4_height_mm, hex_size, offset_y=10)
+grid.connected_segments()
